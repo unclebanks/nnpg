f3ac14fdc6887f0f6d36b57ef09b092bea6cb94d
unclebanks <49108377+unclebanks@users.noreply.github.com>
2023-07-16 13:45:30 -0700

    Updates

--- src/components/ModalPopup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ModalPopup = () => {
-  return (
-    <div id="modalPopup">
-        <span id="popupText">Test text</span>
-    </div>
-  )
-}
--- src/components/PokeMartContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PokeMartContainer = (props) => {
-  return (
-    <div>{props.player.settings.currentRegionId}</div>
-  )
-}
